2a1b60fbad303172057a2c81ca3d7dfc282e3208
AnannaRahman <ananna.rahman92@gmail.com>
2019-02-15 23:15:53 +0600

    Splash Implementation

--- app/src/main/java/com/appwiz/newsviews/Activity/MainActivity.java
+++ app/src/main/java/com/appwiz/newsviews/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.appwiz.newsviews;
+package com.appwiz.newsviews.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.appwiz.newsviews.R;
 
 public class MainActivity extends AppCompatActivity {
 
